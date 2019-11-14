f1e6d12d1953a9625114ff83397574f1b047a1d5
alex <alex@alexs-MacBook-Air.local>
2019-11-14 14:39:03 +0100

    first commit

---
+++ src/main/kotlin/net/sidion/App.kt
@@ -0,0 +1,49 @@
+package net.sidion
+
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.jackson.jackson
+import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import net.sidion.dao.DAOFacadeDatabase
+import net.sidion.model.Address
+import org.jetbrains.exposed.sql.Database
+
+val dao = DAOFacadeDatabase(
+        Database.connect("jdbc:mysql://root:root@localhost:3306/humanbeings_db?useUnicode=true&serverTimezone=UTC",
+                driver = "com.mysql.jdbc.Driver"))
+fun main() {
+    embeddedServer(Netty, port = 8080){
+        dao.init()
+        install(CallLogging)
+        install(ContentNegotiation){
+            jackson {}
+        }
+        routing {
+            route("/employees"){
+                get {
+                    call.respond(dao.getAllAddresses())
+                }
+                post {
+                    val emp = call.receive<Address>()
+                    dao.createAddress(emp.name, emp.email, emp.city)
+                }
+                put {
+                    val emp = call.receive<Address>()
+                    dao.updateAddress(emp.id, emp.name, emp.email, emp.city)
+                }
+                delete("/{id}") {
+                    val id = call.parameters["id"]
+                    if(id != null)
+                        dao.deleteAddress(id.toInt())
+                }
+            }
+        }
+    }.start(wait = true)
+}
+
---
+++ src/main/kotlin/net/sidion/model/Address.kt
@@ -0,0 +1,4 @@
+package net.sidion.model
+
+data class Address(val id: Int, val name: String,
+                   val email: String, val city: String)
